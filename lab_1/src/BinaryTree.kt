ae28ab51ecf53b0b01e88db3a5bb3eaf862976d5
Mialenna <annakupriyanova04@mail.ru>
2024-02-20 00:28:34 +0700

    .

--- lab_1/src/BinaryTree.kt
+++ lab_1/src/BinaryTree.kt
@@ -5,12 +5,16 @@ class Node(
     var left: Node? = null,
     var right: Node? = null) {
 
+    //Значение = значению текущего узла -> конец поиска.
+    //Больше - право, меньше - лево.
     fun find(value: Int): Node? = when {
         this.key > value -> left?.find(value)
         this.key < value -> right?.find(value)
         else -> this
     }
 
+    //Если значение отсуствует = вставить в узел, который имеет свободную "щель".
+    //Присутствует = ничего делать не надо.
     fun insert(value: Int) {
         if (value > this.key) {
             if (this.right == null) {
@@ -27,6 +31,7 @@ class Node(
         }
     }
 
+    //Скан дерева. Классификация по кол-ву ненулевых дочерних узлов.
     fun delete(value: Int) {
         when {
             value > key -> scan(value, this.right, this)
@@ -37,7 +42,7 @@ class Node(
 
     private fun scan(value: Int, node: Node?, parent: Node?) {
         if (node == null) {
-            System.out.println("value" + value)
+            System.out.println("value" + value + "is null")
             return
         }
         when {
@@ -62,6 +67,8 @@ class Node(
         }
     }
 
+    //Если узел является корневым, мы не можем его устранить.
+    //В противном случае достаточно установить null соответствующего потомка родителя.
     private fun removeNoChildNode(node: Node, parent: Node?) {
         if (parent == null) {
             throw IllegalStateException("Can not remove the root node without child nodes")
@@ -73,12 +80,15 @@ class Node(
         }
     }
 
+    //Достаточно «переместить» единственный дочерний узел в узел, который мы удаляем.
     private fun removeSingleChildNode(parent: Node, child: Node) {
         parent.key = child.key
         parent.left = child.left
         parent.right = child.right
     }
 
+    //Мы должны найти узел, который должен заменить узел, который мы хотим удалить.
+    //Выбрать узел в левом поддереве с наибольшим ключом.
     private fun removeTwoChildNode(node: Node) {
         val leftChild = node.left!!
         leftChild.right?.let {
@@ -102,6 +112,7 @@ class Node(
         }
     }
 
+    //Предварительный заказ (посещение родительского узла, затем левого потомка, затем правого потомка)
     fun visit(): Array<Int> {
         val a = left?.visit() ?: emptyArray()
         val b = right?.visit() ?: emptyArray()
@@ -111,8 +122,9 @@ class Node(
     override fun toString(): String {
         return buildString {
             append(this@Node.key.toString())
-            append('\n')
+            append("\nleft ")
             append(left?.toString().orEmpty())
+            append("\nrigth ")
             append(right?.toString().orEmpty())
             append('\n')
         }
--- lab_1/src/Main.kt
+++ lab_1/src/Main.kt
@@ -1,24 +1,18 @@
 fun main() {
+    println("Mobile")
     val phone = MobilePhone("89236092710")
     phone.addContact(Contact("Sveta", "89234562134"))
     phone.addContact(Contact("Daria", "89231654782"))
     phone.addContact(Contact("Nik", "89231115687"))
     phone.printContacts()
+    print("\n")
     phone.updateContact(Contact("Sveta", "89234562134"), Contact("Svetlana", "89236912366"))
-    phone.printContacts()
     phone.removeContact(Contact("Nik", "89231115687"))
     phone.printContacts()
-    println("${phone.findContact(Contact("Daria", "89231654782"))}")
+    println("index = ${phone.findContact(Contact("Daria", "89231654782"))}")
     println("${phone.queryContact("Daria")}")
 
-    /*val tree = Node(2).apply {
-        insert(5)
-        insert(1)
-        insert(3)
-        println(this)
-        delete(3)
-    }*/
-
+    println("\nTree")
     val binaryTree = Node(5)
     binaryTree.insert(3)
     binaryTree.insert(7)
@@ -26,6 +20,8 @@ fun main() {
     binaryTree.insert(4)
     binaryTree.insert(6)
     binaryTree.insert(8)
-    println(binaryTree)
-    binaryTree.delete(8)
+    print(binaryTree)
+    binaryTree.delete(7)
+    print("Tree without 7\n")
+    print(binaryTree)
 }
