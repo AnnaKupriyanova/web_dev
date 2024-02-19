d6964b80e20ac863e6dddadcafe8384277edbc94
Mialenna <annakupriyanova04@mail.ru>
2024-02-20 00:31:23 +0700

    Update Main.kt

--- lab_1/src/Main.kt
+++ lab_1/src/Main.kt
@@ -12,10 +12,7 @@ fun main() {
     println("index = ${phone.findContact(Contact("Daria", "89231654782"))}")
     println("${phone.queryContact("Daria")}")
 
-<<<<<<< HEAD
     println("\nTree")
-=======
->>>>>>> d1239d3dd476b905c7800041a774598a6cbd19f4
     val binaryTree = Node(5)
     binaryTree.insert(3)
     binaryTree.insert(7)
@@ -23,14 +20,8 @@ fun main() {
     binaryTree.insert(4)
     binaryTree.insert(6)
     binaryTree.insert(8)
-<<<<<<< HEAD
     print(binaryTree)
     binaryTree.delete(7)
     print("Tree without 7\n")
     print(binaryTree)
 }
-=======
-    println(binaryTree)
-    binaryTree.delete(8)
-}
->>>>>>> d1239d3dd476b905c7800041a774598a6cbd19f4
